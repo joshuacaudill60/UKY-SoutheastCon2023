73e1782dbd6a3fc729c1483b2b10691d0dcda088
Joshua Caudill <56001713+joshuacaudill60@users.noreply.github.com>
2023-02-09 13:51:00 -0500

    Update auto_start.ino

--- auto_start.ino
+++ auto_start.ino
@@ -58,170 +58,177 @@ void setup() {
 } // END setup
 
 void loop() {
+if (Serial.available()) {
+  char ch = (char)Serial.read(); 
+  switch(ch) {
+    case '9':
+      move_diag_top_right();
+      break;
+      
+    case '8':
+      move_forward();
+      break;
+      
+    case '7':
+      move_diag_top_left();
+      break;
+      
+    case '6':
+      move_right();
+      break;
+      
+    case '5':
+      stopped();
+      break;
+      
+    case '4':
+      move_left();
+      break;
+      
+    case '3':
+      move_diag_bot_right();
+      break;
+      
+    case '2':
+      move_backward();
+      break;
+      
+    case '1':
+      move_diag_bot_left();
+      break; 
+
+    case '>':
+      move_cw();
+      break;
+    
+    case '<':
+      move_ccw();
+      break;
+
+    case '/':
+      move_c();
+      break;
+
+    case '*':
+      move_reset_c();
+      break;
+    
+    case '0':
+      auto_start();
+      break;
+      
+    default:
+      stopped();
+      break;    
+  }
+} 
+} // END loop
+
+// auto start using the red LED
+void auto_start() {
   int A0val = analogRead(A0);
   float voltage= A0val * (5.0 / 1023.0);
   Serial.println(voltage);
   if(voltage > 0.15) {
     move_forward();
-    
   }
-// if (Serial.available()) {
-//   char ch = (char)Serial.read(); 
-//   switch(ch) {
-//     case '9':
-//       move_diag_top_right();
-//       break;
-//       
-//     case '8':
-//       move_forward();
-//       break;
-//       
-//     case '7':
-//       move_diag_top_left();
-//       break;
-//       
-//     case '6':
-//       move_right();
-//       break;
-//       
-//     case '5':
-//       stopped();
-//       break;
-//       
-//     case '4':
-//       move_left();
-//       break;
-//       
-//     case '3':
-//       move_diag_bot_right();
-//       break;
-//       
-//     case '2':
-//       move_backward();
-//       break;
-//       
-//     case '1':
-//       move_diag_bot_left();
-//       break; 
-//
-//     case '>':
-//       move_cw();
-//       break;
-//     
-//     case '<':
-//       move_ccw();
-//       break;
-//
-//     case '/':
-//       move_c();
-//       break;
-//
-//     case '*':
-//       move_reset_c();
-//       break;
-//         
-//     default:
-//       stopped();
-//       break;    
-//   }
-// } 
-} // END loop
+}
 
-//// move the robot forward
+// move the robot forward
 void move_forward() {
   FL.writeMicroseconds (FLForward);
   FR.writeMicroseconds (FRForward);
   BL.writeMicroseconds (BLForward);
   BR.writeMicroseconds (BRForward);
 } // END move_forward
-//
-//// move the robot backward
-//void move_backward() {
-//  FL.writeMicroseconds (FLBackward);
-//  FR.writeMicroseconds (FRBackward);
-//  BL.writeMicroseconds (BLBackward);
-//  BR.writeMicroseconds (BRBackward);
-//} // END move_backward
-//
-//// move the robot left
-//void move_left () {
-//  FL.writeMicroseconds(FLBackward);
-//  FR.writeMicroseconds(FRForward);
-//  BL.writeMicroseconds(BLForward);
-//  BR.writeMicroseconds(BRBackward);
-//} // END move_left
-//
-//// move the robot right
-//void move_right() {
-//  FL.writeMicroseconds(FLForward);
-//  FR.writeMicroseconds(FRBackward);
-//  BL.writeMicroseconds(BLBackward);
-//  BR.writeMicroseconds(BRForward);
-//} // END move_right
-//
-//// move the robot top left
-//void move_diag_top_left() {
-//  FL.writeMicroseconds(FLStopped);
-//  FR.writeMicroseconds(FRForward);
-//  BL.writeMicroseconds(BLForward);
-//  BR.writeMicroseconds(BRStopped);
-//} // END move_diag_top_left
-//
-//// move the robot top right
-//void move_diag_top_right() {
-//  FL.writeMicroseconds(FLForward);
-//  FR.writeMicroseconds(FRStopped);
-//  BL.writeMicroseconds(BLStopped);
-//  BR.writeMicroseconds(BRForward);
-//} // END move_diag_top_right
-//
-//// move the robot bottom left
-//void move_diag_bot_left() {
-//  FL.writeMicroseconds(FLBackward);
-//  FR.writeMicroseconds(FRStopped);
-//  BL.writeMicroseconds(BLStopped);
-//  BR.writeMicroseconds(BRBackward);
-//} // END move_diag_bot_left
-//
-//// move the robot bottom right
-//void move_diag_bot_right() {
-//  FL.writeMicroseconds(FLStopped);
-//  FR.writeMicroseconds(FRBackward);
-//  BL.writeMicroseconds(BLBackward);
-//  BR.writeMicroseconds(BRStopped);
-//} // END move_diag_bot_right
-//
-//// stop the robot
-//void stopped() {
-//  FL.writeMicroseconds (FLStopped);
-//  FR.writeMicroseconds (FRStopped);
-//  BL.writeMicroseconds (BLStopped);
-//  BR.writeMicroseconds (BRStopped);
-//} // END stopped
-//
-//// move the robot clockwise
-//void move_cw() {
-//  FL.writeMicroseconds (FLForward);
-//  FR.writeMicroseconds (FRBackward);
-//  BL.writeMicroseconds (BLForward);
-//  BR.writeMicroseconds (BRBackward);
-//} // END move_cw
-//
-//// move the robot counter clockwise
-//void move_ccw() {
-//  FL.writeMicroseconds (FLBackward);
-//  FR.writeMicroseconds (FRForward);
-//  BL.writeMicroseconds (BLBackward);
-//  BR.writeMicroseconds (BRForward);
-//} // END move_ccw
-//
-//// move the left chip dispenser
-//void move_c() {
-//  CL.writeMicroseconds (CLForward);
-//  CR.writeMicroseconds (CRForward);
-//} // END move_c
-//
-//void move_reset_c() {
-//  CL.writeMicroseconds (CLBackward);
-//  CR.writeMicroseconds (CRBackward);
-//} // END move_reset_c
+
+// move the robot backward
+void move_backward() {
+ FL.writeMicroseconds (FLBackward);
+ FR.writeMicroseconds (FRBackward);
+ BL.writeMicroseconds (BLBackward);
+ BR.writeMicroseconds (BRBackward);
+} // END move_backward
+
+// move the robot left
+void move_left () {
+ FL.writeMicroseconds(FLBackward);
+ FR.writeMicroseconds(FRForward);
+ BL.writeMicroseconds(BLForward);
+ BR.writeMicroseconds(BRBackward);
+} // END move_left
+
+// move the robot right
+void move_right() {
+ FL.writeMicroseconds(FLForward);
+ FR.writeMicroseconds(FRBackward);
+ BL.writeMicroseconds(BLBackward);
+ BR.writeMicroseconds(BRForward);
+} // END move_right
+
+// move the robot top left
+void move_diag_top_left() {
+ FL.writeMicroseconds(FLStopped);
+ FR.writeMicroseconds(FRForward);
+ BL.writeMicroseconds(BLForward);
+ BR.writeMicroseconds(BRStopped);
+} // END move_diag_top_left
+
+// move the robot top right
+void move_diag_top_right() {
+ FL.writeMicroseconds(FLForward);
+ FR.writeMicroseconds(FRStopped);
+ BL.writeMicroseconds(BLStopped);
+ BR.writeMicroseconds(BRForward);
+} // END move_diag_top_right
+
+// move the robot bottom left
+void move_diag_bot_left() {
+ FL.writeMicroseconds(FLBackward);
+ FR.writeMicroseconds(FRStopped);
+ BL.writeMicroseconds(BLStopped);
+ BR.writeMicroseconds(BRBackward);
+} // END move_diag_bot_left
+
+// move the robot bottom right
+void move_diag_bot_right() {
+ FL.writeMicroseconds(FLStopped);
+ FR.writeMicroseconds(FRBackward);
+ BL.writeMicroseconds(BLBackward);
+ BR.writeMicroseconds(BRStopped);
+} // END move_diag_bot_right
+
+// stop the robot
+void stopped() {
+ FL.writeMicroseconds (FLStopped);
+ FR.writeMicroseconds (FRStopped);
+ BL.writeMicroseconds (BLStopped);
+ BR.writeMicroseconds (BRStopped);
+} // END stopped
+
+// move the robot clockwise
+void move_cw() {
+ FL.writeMicroseconds (FLForward);
+ FR.writeMicroseconds (FRBackward);
+ BL.writeMicroseconds (BLForward);
+ BR.writeMicroseconds (BRBackward);
+} // END move_cw
+
+// move the robot counter clockwise
+void move_ccw() {
+ FL.writeMicroseconds (FLBackward);
+ FR.writeMicroseconds (FRForward);
+ BL.writeMicroseconds (BLBackward);
+ BR.writeMicroseconds (BRForward);
+} // END move_ccw
+
+// move the left chip dispenser
+void move_c() {
+ CL.writeMicroseconds (CLForward);
+ CR.writeMicroseconds (CRForward);
+} // END move_c
+
+void move_reset_c() {
+ CL.writeMicroseconds (CLBackward);
+ CR.writeMicroseconds (CRBackward);
+} // END move_reset_c
